b5d3e5f3d9590a3af2799f5612546e3c24cb28eb
Cleyson Silva <cleysonsilva.me@gmail.com>
2024-03-08 00:11:06 -0300

    04-02-pizzashop: setup do projeto

--- projeto-04-02-pizzashop/.eslintrc.cjs
+++ projeto-04-02-pizzashop/.eslintrc.cjs
@@ -1,18 +1,7 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: ['@rocketseat/eslint-config/react'],
+  plugins: ['simple-import-sort'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'simple-import-sort/imports': 'error',
   },
 }
